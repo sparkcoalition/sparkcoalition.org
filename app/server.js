9642f608ae9c1606d42ae32b81d952996cf14962
root <root@ip-10-154-155-204.ec2.internal>
2013-08-30 04:15:00 +0000

    cleanup

---
+++ app/server.js
@@ -0,0 +1,15 @@
+//var winston = require('winston');
+
+var express = require('express');
+var app = express();
+
+app.use(express.bodyParser());
+
+app.use(express.logger('dev'));
+
+app.use(express.static(__dirname+'/.');
+
+app.use(app.router);
+
+app.listen(80);
+
